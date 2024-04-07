086d7467b9464e2b2bc787273b5f8bf48f9d02a7
Jeff Andre Millan <122857926+jeffandre123@users.noreply.github.com>
2024-04-07 18:32:59 +0800

    final

--- src/classes/board.ts
+++ src/classes/board.ts
@@ -1,57 +1,143 @@
 class Board {
+    private tiles: number[][];
+  
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     constructor(tiles: number[][]) {
-        // YOUR CODE HERE
+      this.tiles = tiles;
     }
-
+  
     // string representation of this board
     toString(): string {
-        // PLS MODIFY
-        return "";
+      let result = `${this.dimension()}\n`;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          result += `${this.tiles[row][col]} `;
+        }
+        result += '\n';
+      }
+      return result;
     }
-
+  
     // board dimension n
     dimension(): number {
-        // PLS MODIFY
-        return 0;
+      return this.tiles.length;
     }
-
+  
     // number of tiles out of place
     hamming(): number {
-        // PLS MODIFY
-        return 0;
+      let distance = 0;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] !== row * this.dimension() + col + 1 && this.tiles[row][col] !== 0) {
+            distance++;
+          }
+        }
+      }
+      return distance;
     }
-
+  
     // sum of Manhattan distances between tiles and goal
     manhattan(): number {
-        // PLS MODIFY
-        return 0;
+      let distance = 0;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] !== 0) {
+            const goalRow = Math.floor((this.tiles[row][col] - 1) / this.dimension());
+            const goalCol = (this.tiles[row][col] - 1) % this.dimension();
+            distance += Math.abs(row - goalRow) + Math.abs(col - goalCol);
+          }
+        }
+      }
+      return distance;
     }
-
+  
     // is this board the goal board?
     isGoal(): boolean {
-        // PLS MODIFY
-        return true;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] !== row * this.dimension() + col + 1 && (row !== this.dimension() - 1 || col !== this.dimension() - 1)) {
+            return false;
+          }
+        }
+      }
+      return true;
     }
-
+  
     // does this board equal y?
     equals(y: Board): boolean {
-        // PLS MODIFY
-        return true;
+      if (this.dimension() !== y.dimension()) {
+        return false;
+      }
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] !== y.tiles[row][col]) {
+            return false;
+          }
+        }
+      }
+      return true;
     }
-
+  
     // all neighboring boards
     neighbors(): Board[] {
-        // PLS MODIFY
-        return [];
+      const neighbors: Board[] = [];
+      let blankRow = -1;
+      let blankCol = -1;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] === 0) {
+            blankRow = row;
+            blankCol = col;
+            break;
+          }
+        }
+      }
+      if (blankRow > 0) {
+        const neighbor = this.swap(blankRow, blankCol, blankRow - 1, blankCol);
+        neighbors.push(neighbor);
+      }
+      if (blankRow < this.dimension() - 1) {
+        const neighbor = this.swap(blankRow, blankCol, blankRow + 1, blankCol);
+        neighbors.push(neighbor);
+      }
+      if (blankCol > 0) {
+        const neighbor = this.swap(blankRow, blankCol, blankRow, blankCol - 1);
+        neighbors.push(neighbor);
+      }
+      if (blankCol < this.dimension() - 1) {
+        const neighbor = this.swap(blankRow, blankCol, blankRow, blankCol + 1);
+        neighbors.push(neighbor);
+      }
+      return neighbors;
     }
-
+  
     // a board that is obtained by exchanging any pair of tiles
     twin(): Board {
-        // PLS MODIFY
-        return new Board([[]]);
+      let firstRow = -1;
+      let firstCol = -1;
+      let secondRow = -1;
+      let secondCol = -1;
+      for (let row = 0; row < this.dimension(); row++) {
+        for (let col = 0; col < this.dimension(); col++) {
+          if (this.tiles[row][col] !== 0 && firstRow === -1) {
+            firstRow = row;
+            firstCol = col;
+          } else if (this.tiles[row][col] !== 0 && firstRow !== -1) {
+            secondRow = row;
+            secondCol = col;
+            break;
+          }
+        }
+      }
+      return this.swap(firstRow, firstCol, secondRow, secondCol);
     }
-}
-
-export default Board;
+  
+    private swap(row1: number, col1: number, row2: number, col2: number): Board {
+      const tiles = this.tiles.map((row) => row.slice());
+      [tiles[row1][col1], tiles[row2][col2]] = [tiles[row2][col2], tiles[row1][col1]];
+      return new Board(tiles);
+    }
+  }
+  
+  export default Board;
--- src/classes/solver.ts
+++ src/classes/solver.ts
@@ -1,29 +1,102 @@
-import Board from "./board";
-import { MinHeap } from "min-heap-typed";
+import Board from './board'
+import { MinHeap } from 'min-heap-typed'
 
-class Solver {
-    // find a solution to the initial board (using the A* algorithm)
-    constructor(initial: Board) {
-        // YOUR CODE HERE
-    }
+class SearchNode {
+  board: Board
+  moves: number
+  previous: SearchNode | null
+  priority: number
 
-    // is the initial board solvable? (see below)
-    isSolvable(): boolean {
-        // PLS MODIFY
-        return true;
-    }
-
-    // min number of moves to solve initial board; -1 if unsolvable
-    moves(): number {
-        // PLS MODIFY
-        return 0;
-    }
-
-    // sequence of boards in a shortest solution; null if unsolvable
-    solution(): Board[] {
-        // PLS MODIFY
-        return [];
-    }
+  constructor (board: Board, moves: number, previous: SearchNode | null) {
+    this.board = board
+    this.moves = moves
+    this.previous = previous
+    this.priority = this.board.manhattan() + this.moves
+  }
 }
 
-export default Solver;
+class Solver {
+  private initialSearchNode: SearchNode
+  private goalSearchNode: SearchNode | null = null
+  private solvable: boolean
+
+  // find a solution to the initial board (using the A* algorithm)
+  constructor (initial: Board) {
+    if (initial === null) {
+      throw new Error('Initial board cannot be null')
+    }
+
+    this.initialSearchNode = new SearchNode(initial, 0, null)
+    this.solvable = this.solve()
+  }
+
+  // is the initial board solvable? (see below)
+  isSolvable (): boolean {
+    return this.solvable
+  }
+
+  // min number of moves to solve initial board; -1 if unsolvable
+  moves (): number {
+    return this.solvable ? this.goalSearchNode!.moves : -1
+  }
+
+  // sequence of boards in a shortest solution; null if unsolvable
+  solution (): Board[] {
+    if (!this.solvable) {
+      return []
+    }
+
+    const solution: Board[] = []
+    let current: SearchNode | null = this.goalSearchNode
+    while (current !== null) {
+      solution.unshift(current.board)
+      current = current.previous
+    }
+    return solution
+  }
+
+  private solve (): boolean {
+    const pq = new MinHeap<SearchNode>()
+    pq.add(this.initialSearchNode)
+
+    const twinPq = new MinHeap<SearchNode>()
+    const twinInitial = this.initialSearchNode.board.twin()
+    twinPq.add(new SearchNode(twinInitial, 0, null))
+
+    while (!pq.isEmpty() && !twinPq.isEmpty()) {
+      const node = pq.poll()
+      if (node && node.board.isGoal()) {
+        this.goalSearchNode = node
+        return true
+      }
+
+      if (node) {
+        for (const neighbor of node.board.neighbors()) {
+          if (node.previous === null || !neighbor.equals(node.previous.board)) {
+            pq.add(new SearchNode(neighbor, node.moves + 1, node))
+          }
+        }
+      }
+
+      const twinNode = twinPq.poll()
+      if (twinNode && twinNode.board.isGoal()) {
+        return false
+      }
+
+      if (twinNode) {
+        for (const neighbor of twinNode.board.neighbors()) {
+          if (
+            twinNode.previous === null ||
+            !neighbor.equals(twinNode.previous.board)
+          ) {
+            twinPq.add(new SearchNode(neighbor, twinNode.moves + 1, twinNode))
+          }
+        }
+      }
+    }
+
+    return false
+  }
+}
+
+export default Solver
